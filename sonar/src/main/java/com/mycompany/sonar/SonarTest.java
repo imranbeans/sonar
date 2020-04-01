e557b62c297af2aacc5ff1fb5418f52176d75515
imranbeans <62978117+imranbeans@users.noreply.github.com>
2020-04-01 12:43:51 +0800

    Add files via upload

---
+++ sonar/src/main/java/com/mycompany/sonar/SonarTest.java
@@ -0,0 +1,40 @@
+package com.mycompany.sonar;
+
+public class SonarTest {
+
+    public static void main(String[] args) {
+        SonarTest sonar = new SonarTest();
+        sonar.doSomeThing();
+        sonar.checkLeapYear(2011);
+    }
+
+    public void doSomeThing() {
+        System.out.println("Welcome to Vector Gate.");
+    }
+
+    private void checkLeapYear(int year) {
+        boolean leap = false;
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                // year is divisible by 400, hence the year is a leap year
+                if (year % 400 == 0) {
+                    leap = true;
+                } else {
+                    leap = false;
+                }
+            } else {
+                leap = true;
+            }
+        } else {
+            leap = false;
+        }
+
+        if (leap) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+
+}
